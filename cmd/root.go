ac8a5445fefbd21ca4284d6a3ab87bfd1aa4cf69
Emanuel Bennici <eb@fabmation.de>
2020-02-17 18:27:17 +0100

    Add rootCmd in cmd/
    We are switching to are more convenient project structure layout and
    also replacing the `flag` library with `cobra`.
    This is because – maybe in the future – there will we a CLI to manage
    the kubenab configuration, getting stats, debugging ...

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/jfrog/kubenab/internal"
+	"github.com/spf13/cobra"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "kubenab",
+	Short: "K8s Image policy enforcer",
+	// TODO: This 'long' description needs to be enhanced!
+	Long: `Kubernetes Admission Webhook to enforce pulling of Docker images from the private registry.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is /etc/kubenab/config.yaml)")
+	rootCmd.PersistentFlags().BoolVar(&internal.Debug, "debug", false, "Print Debug Messages (defaults to false)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath("/etc/kubenab")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
