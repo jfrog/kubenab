37aee04ac0903e76c5581a4fe00a01bc1f2b4aaa
Emanuel Bennici <eb@fabmation.de>
2020-03-10 17:51:10 +0100

    Add 'serve' command and flags

---
+++ cmd/serve.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2020 Francesco Emanuel Bennici <benniciemanuel78@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the kubenab webhook server",
+	Long: `Starts a HTTP(s) webserver which handels the
+Mutating- and Validating-Webhook.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	serveCmd.Flags().StringVar(&internal.ListenAddr, "listen", "0.0.0.0", "listen address")
+	serveCmd.Flags().StringVar(&internal.ListenPort, "port", "8080", "listen port")
+}
--- internal/flags.go
+++ internal/flags.go
@@ -1,9 +1,16 @@
 package internal
 
+// this variables will be set on compilation-time.
 var (
-	Debug      bool   // Debug describes if the debug lvl is enabled or not
 	DebugAvail bool   // DebugAvail describes if the binary was compiled with debug features enabled.
 	Version    string // Version contains the current version string and will be set on compile time
 	Commit     string // Commit contains the latest git commit hash
 	BuildDate  string // BuildDate contains the date (UTC) when the binary was built
+)
+
+// this variables are used for saving flag arguments
+var (
+	Debug      bool   // Debug describes if the debug lvl is enabled or not
+	ListenAddr string // ListenAddr contains the address where it should listen
+	ListenPort uint   // ListenPort contains the port where it should listen
 )
