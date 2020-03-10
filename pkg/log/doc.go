0b995b50b7e3bafde958911fad0d14c307216c7b
Emanuel Bennici <eb@fabmation.de>
2020-03-10 17:20:59 +0100

    Move 'log' package documentation to doc.go

---
+++ pkg/log/doc.go
@@ -0,0 +1,4 @@
+// log package is a wrapper around the current log package to provide the
+// possibility to "disable" Debug log output => GoLang compiler will
+// remove it from the binary.
+package log
--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,6 +1,3 @@
-// log package is a wrapper around the current log package to provide the
-// possibility to "disable" Debug log output => GoLang compiler will
-// remove it from the binary.
 package log
 
 import "log"
