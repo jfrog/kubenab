244fde8898be072cb00581fc4f77a0720e50a956
Emanuel Bennici <eb@fabmation.de>
2020-03-10 17:41:55 +0100

    Format 'internal' pkg

--- internal/flags.go
+++ internal/flags.go
@@ -1,8 +1,9 @@
 package internal
 
 var (
-	Debug     bool   // Debug describes if the debug lvl is enabled or not
-	Version   string // Version contains the current version string and will be set on compile time
-	Commit    string // Commit contains the latest git commit hash
-	BuildDate string // BuildDate contains the date (UTC) when the binary was built
+	Debug      bool   // Debug describes if the debug lvl is enabled or not
+	DebugAvail bool   // DebugAvail describes if the binary was compiled with debug features enabled.
+	Version    string // Version contains the current version string and will be set on compile time
+	Commit     string // Commit contains the latest git commit hash
+	BuildDate  string // BuildDate contains the date (UTC) when the binary was built
 )
