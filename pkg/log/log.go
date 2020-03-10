e26d8036b03dcb772dc79316f68dc103874ddbc5
Emanuel Bennici <eb@fabmation.de>
2020-03-10 17:19:51 +0100

    Correct the 'log' package documentation
    The old documentation of the 'log' package was not conform to the GoDocs
    guidlines.

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,5 +1,6 @@
-// Why a special log Package? Because if we compile a Release we do not need
-// the debug Output.
+// log package is a wrapper around the current log package to provide the
+// possibility to "disable" Debug log output => GoLang compiler will
+// remove it from the binary.
 package log
 
 import "log"
